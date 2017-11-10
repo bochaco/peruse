55e547908e0007d68b9407e17cc73cd558ff15b3
Josh Wilson <joshuef@gmail.com>
2017-11-10 18:31:48 +0100

    more live dev stuf

--- webpack.config.renderer.live.dev.js
+++ webpack.config.renderer.live.dev.js
@@ -78,7 +78,8 @@ export default merge.smart( baseConfig, {
      * 'staging', for example, by changing the ENV variables in the npm scripts
      */
         new webpack.DefinePlugin( {
-            'process.env.NODE_ENV' : JSON.stringify( process.env.NODE_ENV || 'production' )
+            'process.env.NODE_ENV' : JSON.stringify( process.env.NODE_ENV || 'production' ),
+            'process.env.DEBUG_PROD' : true
         } ),
 
         new webpack.LoaderOptionsPlugin( {
